2fbb99963d850ef576655476e90fc59016c552bf
Pranith Rao <pranithrao3@gmail.com>
2023-06-28 22:29:08 +0530

    Backend & DB Setup Done

---
+++ api/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect('mongodb://127.0.0.1:27017/BlogVoyage', {
+	useNewUrlParser: true, 
+	useUnifiedTopology: true
+}).then(() => console.log("Connected to MongoDB")).catch(console.error);
+
+app.listen(3001);
